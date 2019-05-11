1e9a2653dac986a7b9ec853048bac605b43f3c77
Julia.Juszczak <Julia@Juszczak.info>
2019-05-11 11:04:46 +0200

    kategorie pojazdów

--- src/main/java/pl/sda/rav/Motorboat.java
+++ src/main/java/pl/sda/rav/Motorboat.java
@@ -1,4 +1,7 @@
 package pl.sda.rav;
 
 public class Motorboat extends Vehicle {
+    public Motorboat(int vin, String name, String productionDate) {
+        super(vin, name, productionDate);
+    }
 }
